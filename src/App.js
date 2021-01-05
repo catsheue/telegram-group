9f31a6e373a7a96b1a2eff9bd34b37c3d0855121
luna.lee <luna.lee@nextfortune.tw>
2021-01-05 19:59:42 +0800

    [Add] init

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <div>
+        Telegram Groups
+      </div>
+        <div><a href="">Github</a></div>
       </header>
+
+      <div className="App-body">
+        <nav>
+          <ul>
+            <li></li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
